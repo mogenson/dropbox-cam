2a9d7c22b38e200a327e0a16c6c4e02a87c76a24
Michael Mogenson <michael.mogenson@gmail.com>
2017-05-30 12:35:25 -0400

    Don't let the IMAGE_DIR size exceed IMAGE_DIR_SIZE
    Specify a max image directory size in gigabytes. Convert this to bytes
    and check to see if the image directory is larger than this value. If it
    is, delete the oldest image until the image directory is small enough.

--- dropbox-cam.sh
+++ dropbox-cam.sh
@@ -3,6 +3,7 @@
 HOSTS_FILE="$HOME/.dropbox-cam-hosts"
 IMAGE_DIR="$HOME/images/"
 IMAGE_NAME="image-$(date +%Y-%m-%d-%R).jpg"
+IMAGE_DIR_SIZE=$(echo 24G | numfmt --from=iec)
 
 while read hostname
 do
@@ -14,8 +15,10 @@ raspistill -n -rot 90 -o $IMAGE_DIR$IMAGE_NAME
 
 dropbox_uploader.sh upload $IMAGE_DIR$IMAGE_NAME "/image.jpg"
 
-# 8000 images is about 24 GB
-if [[ $(ls $IMAGE_DIR | wc -l) -gt 8000 ]]
-then
-    rm -v $IMAGE_DIR$(ls $IMAGE_DIR -t | tail -1)
-fi
+# delete the oldest file in IMAGE_DIR until the directory size is
+# less than or equal to IMAGE_DIR_SIZE
+# directory size is measured in bytes, IMAGE_DIR_SIZE is converted to bytes
+while [[ $(du -B 1 $IMAGE_DIR | cut -f 1) -gt $IMAGE_DIR_SIZE ]]
+do
+    rm $IMAGE_DIR$(ls $IMAGE_DIR -t | tail -1)
+done
